246d48acb44a315a1504d0a654b6a8c44fa15749
sathwik2000 <34467058+sathwik2000@users.noreply.github.com>
2020-08-08 02:53:44 +0530

    Update settings.py
    Key

--- Hackvoting/settings.py
+++ Hackvoting/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cii@dq7#msw0_lcydm$o%kj^%t)1pfh11$k=on7@!0(+upst75'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
